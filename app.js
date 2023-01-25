b8aed377598bb605d3713e8eeb25869a7ebfaaf5
kendea1221 <kenshirotamata@gmail.com>
2023-01-25 09:42:28 +0900

    Upload Files

---
+++ app.js
@@ -0,0 +1,32 @@
+//. app.js
+
+//. express
+const express = require( 'express' ),
+      app = express();
+
+//. ejs
+const ejs = require( 'ejs' );
+app.set( 'views', __dirname + '/views' );  
+app.set( 'view engine', 'ejs' );
+
+//. i18n
+const i18n = require( 'i18n' );
+i18n.configure({
+    locales: ['ja' , 'en'],
+    directory: __dirname + '/locales'
+})
+
+//. localhost
+app.get( '/' , function ( req , res ){
+    res.render( ' index ' ,  {} );
+});
+
+//.localhost/sub
+app.get( '/sub' , function ( req , res ){
+    res.render(  'sub ' , {} );
+});
+
+//. port
+const port = process.env.PORT || 3000;
+app.listen( port );
+console.log(" Server starting on " + port + ".... ")
